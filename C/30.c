bb6260b173ef7b271f691e5ba7729847e361a24a
ShreyasAndurlekar <and.shreyas@gmail.com>
2024-10-06 14:02:53 +0530

    output30.c

--- C/30.c
+++ C/30.c
@@ -27,6 +27,36 @@
 // -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
 // coordinates contains no duplicate point.
 
+
+#include <stdio.h>
+#include <stdbool.h>
+
 bool checkStraightLine(int** coordinates, int coordinatesSize, int* coordinatesColSize) {
+    int x0 = coordinates[0][0], y0 = coordinates[0][1];
+    int x1 = coordinates[1][0], y1 = coordinates[1][1];
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    for (int i = 2; i < coordinatesSize; i++) {
+        int x = coordinates[i][0], y = coordinates[i][1];
+        if (dy * (x - x0) != dx * (y - y0)) {
+            return false; 
+        }
+    }
+    return true;  
+}
+
+int main() {
+   
+    int coordinates1[][2] = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+    int coordinates2[][2] = {{1, 1}, {2, 2}, {3, 4}, {4, 5}, {5, 6}, {7, 7}};
     
+    int* arr1[6]; for (int i = 0; i < 6; i++) arr1[i] = coordinates1[i];
+    int* arr2[6]; for (int i = 0; i < 6; i++) arr2[i] = coordinates2[i];
+
+ 
+    printf("%s\n", checkStraightLine(arr1, 6, NULL) ? "true" : "false");  
+    printf("%s\n", checkStraightLine(arr2, 6, NULL) ? "true" : "false");  
+
+    return 0;
 }
